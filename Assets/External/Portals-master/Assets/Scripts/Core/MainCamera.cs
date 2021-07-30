61420bfae8299252d1ee83ba82d9930769bf0981
yaakgold <yaakovgoldberg007@gmail.com>
2021-07-30 16:38:57 -0600

    Black and white shader, and not touching the other portal shader

--- Assets/External/Portals-master/Assets/Scripts/Core/MainCamera.cs
+++ Assets/External/Portals-master/Assets/Scripts/Core/MainCamera.cs
@@ -1,11 +1,33 @@
 ﻿using UnityEngine;
 
-public class MainCamera : MonoBehaviour {
+public class MainCamera : MonoBehaviour
+{
+    public Material normalMat;
+    public bool isBWMode = false;
+    public Shader bwShader;
+    Material bwMat;
 
     Portal[] portals;
 
     void Awake () {
         portals = FindObjectsOfType<Portal> ();
+    }
+
+    void Start()
+    {
+        bwMat = new Material(bwShader);
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if(isBWMode)
+        {
+            Graphics.Blit(src, dst, bwMat);
+        }
+        else
+        {
+            Graphics.Blit(src, dst);
+        }
     }
 
     void OnPreCull () {
