a5cd51120a37cf999b95bdef9a9ee61f0538c64a
snajera531 <salemnajera@gmail.com>
2021-07-31 12:38:07 -0600

    buttons are connected for ui :)

--- Assets/Scripts/UIManagement.cs
+++ Assets/Scripts/UIManagement.cs
@@ -5,38 +5,44 @@ using UnityEngine;
 
 public class UIManagement : MonoBehaviour
 {
-    GameObject creditsPanel;
-    GameObject mainMenuPanel;
+    public GameObject creditsPanel;
+    public GameObject mainMenuPanel;
 
-    void Start()
+    public void Start()
     {
         creditsPanel.SetActive(false);
         mainMenuPanel.SetActive(true);
     }
 
     //scene management
-    void MainMenuScene()
+    public void MainMenuScene()
     {
         //scene 0 will be main menu scene
         SceneManager.LoadScene(0);
     }
 
-    void StartGame()
+    public void StartGame()
     {
         //scene 1 will be game in build settings
         SceneManager.LoadScene(1);
     }
 
     //panel management
-    void Credits()
+    public void Credits()
     {
         creditsPanel.SetActive(true);
         mainMenuPanel.SetActive(false);
     }
 
-    void MainMenuPanel()
+    public void MainMenuPanel()
     {
         creditsPanel.SetActive(false);
         mainMenuPanel.SetActive(true);
+    }
+
+    //game management
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
