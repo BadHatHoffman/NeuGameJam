8edc5a8c48c8f8f9abbc0edbd67b8543f8ca11af
snajera531 <salemnajera@gmail.com>
2021-08-01 12:32:23 -0600

    health bar!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] sounds;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CueAudio(int soundIndex)
+    {
+        switch (soundIndex)
+        {
+            case 0:
+                AudioManager.Instance.Play("MenuSong");
+                break;
+            case 1:
+                AudioManager.Instance.Play("WASDJosh");
+                break;
+            case 2:
+                AudioManager.Instance.Play("JumpJosh");
+                break;
+            case 3:
+                AudioManager.Instance.Play("RunJosh");
+                break;
+            case 4:
+                AudioManager.Instance.Play("SpellJosh");
+                break;
+            case 5:
+                AudioManager.Instance.Play("HurtJosh");
+                break;
+            case 6:
+                AudioManager.Instance.Play("ThrowingJosh");
+                break;
+            case 7:
+                AudioManager.Instance.Play("JumpingJosh");
+                break;
+            case 8:
+                AudioManager.Instance.Play("ConfusedJosh");
+                break;
+            case 9:
+                AudioManager.Instance.Play("WhereIsJosh");
+                break;
+            case 10:
+                AudioManager.Instance.Play("NeedToGetHomeJosh");
+                break;
+            case 11:
+                AudioManager.Instance.Play("FixItJosh");
+                break;
+            case 12:
+                AudioManager.Instance.Play("WhoIsThatJosh");
+                break;
+            case 13:
+                AudioManager.Instance.Play("ColorlessJosh");
+                break;
+            default:
+                break;
+        }
     }
 }
