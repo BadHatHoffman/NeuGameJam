f05ec2679ac039f1826dbc5a8b46defe6ebaed66
snajera531 <salemnajera@gmail.com>
2021-08-01 14:08:33 -0600

    touching things

--- Assets/Scripts/Player/MainController.cs
+++ Assets/Scripts/Player/MainController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class MainController : MonoBehaviour
 {
     public FPSController fpsController;
+    public int totalEntries = 0;
     public int damage;
     public Health health;
     public Vector3 toVec;
