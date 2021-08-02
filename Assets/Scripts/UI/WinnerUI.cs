8dc8a16563c4b8860c3280e3059bd720c44bde67
BadHatHoffman <EHoffman@student.neumont.edu>
2021-08-01 20:37:41 -0600

    asdf

---
+++ Assets/Scripts/UI/WinnerUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinnerUI : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
